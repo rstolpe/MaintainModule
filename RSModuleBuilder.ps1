efb6fc5ee8546de3e3e1fc233c82f1d78bf3b042
Robin Stolpe <robin@stolpe.io>
2022-12-01 08:14:33 +0100

    update

--- RSModuleBuilder.ps1
+++ RSModuleBuilder.ps1
@@ -123,22 +123,22 @@ Write-Verbose "Setting the placeholders for $($outPSDFile)"
 Set-Content -Path $outPSDFile -Value $PSDfileContent -Encoding utf8BOM -Force
 
 Write-Output "Running PSScriptAnalyzer on $($MigrateFunction.name)..."
-<#$ResultPS1 = foreach ($ps1 in $MigrateFunction.FullName) {
+$ResultPS1 = foreach ($ps1 in $MigrateFunction.FullName) {
     write-output "Är på fil $($ps1)"
     if ($null -ne $ps1) {
         $ps1Name = $ps1 -split "/" -replace ".ps1" | Select-Object -Last 1
         Write-Verbose "Running PSScriptAnalyzer on $($ps1Name).ps1..."
         $PSAnalyzerPS1 = Invoke-ScriptAnalyzer -Path $ps1 -ReportSummary
-        #if ($null -ne $PSAnalyzerPS1) {
-        #    $PSAnalyzerPS1 | select-object * | Out-File -Encoding UTF8BOM -FilePath $(Join-Path -Path $TestPath -ChildPath "PSScriptAnalyzer_$($ps1Name)_$($TodaysDate).md")
-        #}
-        #else {
-        #    Write-Output "0 rule violations found." | Out-File -Encoding UTF8BOM -FilePath $(Join-Path -Path $TestPath -ChildPath "PSScriptAnalyzer_$($ps1Name)_$($TodaysDate).md")
-        #}
+        if ($null -ne $PSAnalyzerPS1) {
+            $PSAnalyzerPS1 | select-object * | Out-File -Encoding UTF8BOM -FilePath $(Join-Path -Path $TestPath -ChildPath "PSScriptAnalyzer_$($ps1Name)_$($TodaysDate).md")
+        }
+        else {
+            Write-Output "0 rule violations found." | Out-File -Encoding UTF8BOM -FilePath $(Join-Path -Path $TestPath -ChildPath "PSScriptAnalyzer_$($ps1Name)_$($TodaysDate).md")
+        }
         $PSAnalyzerPS1
     }
 }
-
+<#
 Write-Output "Running PSScriptAnalyzer on $($outPSDFile) and $($outPSMFile)..."
 $CheckPSA = @($outPSDFile, $outPSMFile)
 $ResultPSDPSM = foreach ($file in $CheckPSA) {
