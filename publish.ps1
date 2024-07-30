49d7be46c76e8f27f5c303a8a3479cf60d66c100
Robin Stolpe <robin@stolpe.io>
2024-07-30 08:56:31 +0200

    update

--- publish.ps1
+++ publish.ps1
@@ -1,15 +1,2 @@
-[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
-
-$CheckIfTrusted = Get-PSRepository -name PSGallery | Select-Object InstallationPolicy -ExpandProperty InstallationPolicy
-if ($CheckIfTrusted -eq "Untrusted") {
-    try {
-        Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-    }
-    catch {
-        Write-Error "$($PSItem.Exception)"
-        continue
-    }
-}
-
 $ModulePath = "$PSScriptRoot\MaintainModule"
 Publish-Module -Path $ModulePath -NuGetApiKey $Env:PSGALLERY
